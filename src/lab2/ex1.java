08a89925c41f549c6dc42b91a21c4e1411535b20
Matchima <mukyeunnuwong@gmail.com>
2018-11-26 14:34:53 +0700

    lab 2: Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,42 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+     //integer
+     int a;
+     a=10;
+     System.out.println(a);
+     int b = 20;
+     b=20;
+     System.out.println(b);
+     int c;
+     c = a+b;
+     System.out.println("ผลรวมของ a และ b เท่ากับ\t "+c);
+
+     //real number
+        double m;
+        m = 5.5;
+        m = 5.5*10;
+        System.out.println(m);
+
+        //character
+        char d = '#';
+        System.out.println(d);
+
+        //String
+        String str = "Matchima yeunnuwong";
+        System.out.println("My name is \t"+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+
+
+
+
+    }//main
+
+
+
+}//class
